c63d89b989baa13293a1894b0a0b133dedfade7a
Takayuki Hirayama <hirayama@fiilse.com>
2016-07-05 16:47:20 +0900

    faze1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController 
-	
+	before_action :logged_in_user, only: [:edit, :update]	
   def show
 	  @user = User.find(params[:id])
   end
@@ -40,4 +40,12 @@ class UsersController < ApplicationController
 	  params.require(:user).permit(:name, :email, :region, :password,
 				      :password_confirmation)
   end
+  
+
+  
+
+
+
+
+
  end

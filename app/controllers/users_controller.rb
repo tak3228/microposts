2db334cf25bd6da30d2374a60b57bee6158f26b6
Takayuki Hirayama <hirayama@fiilse.com>
2016-07-05 22:12:55 +0900

    recomand

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 	before_action :correct_user, only: [:edit, :update]	
   def show
 	  @user = User.find(params[:id])
-	  @micropsts = @user.microposts.order(created_at: :desc)
+	  @microposts = @user.microposts.order(created_at: :desc)
   end
 
   def new

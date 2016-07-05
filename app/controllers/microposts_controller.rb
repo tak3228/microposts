86c0094c0962c82b2e5c955c33e972b0c33e7252
Takayuki Hirayama <hirayama@fiilse.com>
2016-07-05 22:21:23 +0900

    destory added

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,5 @@
 class MicropostsController < ApplicationController
-	before_action :logged_in_user, only: [:create]
+	before_action :logged_in_user, only: [:create, :destroy]
 
 	def create
 		@micropost = current_user.microposts.build(micropost_params)
@@ -9,6 +9,14 @@ class MicropostsController < ApplicationController
 		else
 			render 'static_pages/home'
 		end
+	end
+
+	def destroy
+		@micropost = current_user.microposts.find_by(id: params[:id])
+		return redirect_to root_url if @micropost.nil?
+		@micropost.destroy
+		flash[:success] = "Micropost deleted"
+		redirect_to request.referrer || root_url
 	end
 
 	private
